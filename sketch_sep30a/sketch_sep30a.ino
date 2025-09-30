55930ec0f4062d635674efbc2c45f19e9f2fac74
RafaelFr4nklin <rafael_f_de-farias@estudante.sesisenai.org.br>
2025-09-30 10:11:57 -0300

    codigo meia a boca

--- sketch_sep30a/sketch_sep30a.ino
+++ sketch_sep30a/sketch_sep30a.ino
@@ -1,4 +1,16 @@
+#include <WiFi.h>
+WiFiClient client;
+const String SSID = "nome da rede";
+const String PASS = "senha da rede";
+
 void setup() {
+  Serial.begin(115200);
+  client.begin(SSID, PASS);
+  serial.println("conectando no WiFi");
+  while(client.satus()!= WL_CONNECTED) {
+    serial.print(".");
+    delay(200);
+  }
   // put your setup code here, to run once:
 
 }
