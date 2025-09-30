c8f42a670c0a524f2e57f3233fedb11d148a5054
helenacani2 <you@example.com>
2025-09-30 10:13:45 -0300

    Código da aula

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,11 +1,19 @@
 #include <WiFi.h>
+WiFiClient client;
+const String SSID = "Nome da rede";
+const String PASS = "Senha da rede";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  client.begin(SSID, PASS);
+  Serial.println("Conectando no WiFi");
+  while(client.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
