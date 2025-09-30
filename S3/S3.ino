04a316130718ee1f5f2cc95c36d9aeb564e9b6a9
andreerfr <andreerci@gmail.com>
2025-09-30 10:15:15 -0300

    mudança do S3

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,5 +1,18 @@
+#include <WiFi.h>
+WiFiClient client;
+const String SSID = "nome da rede";
+const String PASS = "senha da rede";
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+client.begin(SSID, PASS);
+Serial.println("Conectando no WiFi");
+while(client.status()!=WL_CONNECTED) {
+Serial.print(".");
+delay(200);
+
+}
 
 }
 
