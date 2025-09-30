d26833e8a78a37cc53b49229ca2c60c5604878be
Iago-KK <iago_kleestadt@estudante.sesisenai.org.br>
2025-09-30 10:17:52 -0300

    Atualização do arquivo

--- Trem HIRA/sketch_sep30a/sketch_sep30a.ino
+++ Trem HIRA/sketch_sep30a/sketch_sep30a.ino
@@ -1,9 +1,30 @@
+#include <WiFi.h>
+WifFiClient client;
+
+const String SSID = "nome da rede";
+const Strinf PASS = "senha da rede";
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+
+  client.begin(SSID, PASS);
+
+  Serial.println("Conectando no Wifi");
+
+  while(client.status() != WL_CONNECTED) {
+
+    Serial.print(".");
+    delay(200);
+
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
 
 }
