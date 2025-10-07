f910409f8f33be28f2a922bed1c8b0183ce641c1
andreerfr <andreerci@gmail.com>
2025-10-07 10:09:47 -0300

    S3 modificado

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,22 +1,41 @@
 #include <WiFi.h>
-WiFiClient client;
-const String SSID = "nome da rede";
-const String PASS = "senha da rede";
+#include <PubSubClient.h>
 
+WiFiClient wifi_client;
+PubSubClient mqtt(wifi_client);
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "";
+const String brokerPass ="";
 
 void setup() {
   Serial.begin(115200);
-client.begin(SSID, PASS);
+WiFi.begin(SSID, PASS);
 Serial.println("Conectando no WiFi");
-while(client.status()!=WL_CONNECTED) {
+while(WiFi.status()!=WL_CONNECTED) {
 Serial.print(".");
 delay(200);
 
 }
-
+Serial.println("Conectado com sucesso!");
+mqtt.setServer(brokerURL.c_str(),brokerPort);
+String clientID = "S3-";
+clientID += String(random(0xffff),HEX);
+Serial.println("Conectando ao broker...");
+while(mqtt.connect(clientID.c_str()) == 0){
+  Serial.print(".");
+  delay(200);
+}
+Serial.println("\nConectado ao broker!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
