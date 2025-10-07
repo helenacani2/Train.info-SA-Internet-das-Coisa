37a73eb71c63606ec2b70ddbe1543880ecefde1f
Iago-KK <iago_kleestadt@estudante.sesisenai.org.br>
2025-10-07 09:42:17 -0300

    Conecta ao WiFi e Broker

--- Trem HIRA/sketch_sep30a/sketch_sep30a.ino
+++ Trem HIRA/sketch_sep30a/sketch_sep30a.ino
@@ -1,25 +1,51 @@
 #include <WiFi.h>
-WifFiClient client;
+#include <PubSubClient.h>
 
-const String SSID = "nome da rede";
-const Strinf PASS = "senha da rede";
+WiFiClient wifi_client;
 
+PubSubClient mqtt(wifi_client);
 
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "";
+const String brokerPass = "";
 
 void setup() {
 
   Serial.begin(115200);
 
-  client.begin(SSID, PASS);
+  WiFi.begin(SSID, PASS);
 
   Serial.println("Conectando no Wifi");
 
-  while(client.status() != WL_CONNECTED) {
+  while(WiFi.status() != WL_CONNECTED) {
 
     Serial.print(".");
     delay(200);
 
   }
+
+  Serial.println("Conectado com sucesso");
+
+  mqtt.setServer(brokerURL.c_str(),brokerPort);
+
+  String clientID = "HIRA";
+  clientID += String(random(0xffff),HEX);
+
+  Serial.println("Conectando ao Broker");
+
+  while(mqtt.connect(clientID.c_str()) == 0) {
+
+    Serial.print(".");
+    delay(200);
+
+  }
+
+  Serial.println("\nConectado ao Broker");
 
 }
 
