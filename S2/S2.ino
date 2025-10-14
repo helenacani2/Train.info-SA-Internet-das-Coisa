95883d43a6adf4c60a3186a3c58abce5de5af2f6
RafaelFr4nklin <rafael_f_de-farias@estudante.sesisenai.org.br>
2025-10-14 10:12:33 -0300

    Rafael n sabe fazer commit

--- S2/S2.ino
+++ S2/S2.ino
@@ -9,6 +9,7 @@ const String PASS = "8120gv08"; // senha da rede
 
 const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
+const String topic = "brag";
 
 const String brokerUser = "";
 const String brokerPass = "";
@@ -31,11 +32,33 @@ while (mqtt.connect(ClientID.c_str()) == 0) {
 Serial.print(".");
 delay(200);
 }
+mqtt.subscribe(topic.c_str());
+mqtt.setCallback(callback);
 Serial.println("\nConectado ao broker!");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  String mensagem = "";
+  if(Serial.available() > 0) {
+    mensagem = Serial.readStringUntil('\n');
+    Serial.println("A mensagem foi: ");
+    Serial.println(mensagem);
+    mensagem = "Rafa: " + mensagem;
+    mqtt.publish("brag1", mensagem.c_str());
 
+
+  }
+    mqtt.loop();
 }
+void callback(char* topic, byte* payload, unsigned long length) {
+
+String mensagemRecebida = "";
+for(int i = 0; i < length; i++) {
+  mensagemRecebida += (char) payload[i];
+}
+Serial.println(mensagemRecebida);
+}
+
+
