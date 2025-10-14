57a4a7915c3d7c13b5dbfb8ad6224aee22d55749
Iago-KK <iago_kleestadt@estudante.sesisenai.org.br>
2025-10-14 10:09:27 -0300

    envia mensagens

--- HIRA/HIRA.ino
+++ HIRA/HIRA.ino
@@ -10,6 +10,7 @@ const String PASS = "8120gv08";
 
 const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
+const String topic = "deltarune";
 
 const String brokerUser = "";
 const String brokerPass = "";
@@ -17,7 +18,7 @@ const String brokerPass = "";
 void setup() {
 
   Serial.begin(115200);
-
+  pinMode(2,OUTPUT);
   WiFi.begin(SSID, PASS);
 
   Serial.println("Conectando no Wifi");
@@ -45,12 +46,60 @@ void setup() {
 
   }
 
+  mqtt.subscribe(topic.c_str());
+
+  mqtt.setCallback(callback);
+
   Serial.println("\nConectado ao Broker");
 
 }
 
+
+
 void loop() {
 
+  String mensagem = "";
 
+  if(Serial.available() > 0) {
+
+    mensagem = Serial.readStringUntil('\n');
+
+    mensagem = "Nome: " + mensagem;
+
+    Serial.println(mensagem);
+
+    mqtt.publish("undertale", mensagem.c_str());
+
+  }
+
+  mqtt.loop();
+
+}
+
+
+
+void callback(char* topic, byte* payload, unsigned long length) {
+
+  String MensagemRecebida = "";
+
+  for(int i = 0; i < length; i++) {
+
+    MensagemRecebida += (char) payload[i];
+
+  }
+
+  Serial.println(MensagemRecebida);
+
+  if(MensagemRecebida == "Helena: Acenda") {
+
+    digitalWrite(2, true);
+
+  }
+
+  if(MensagemRecebida == "Helena: Apague") {
+
+    digitalWrite(2, false);
+
+  }
 
 }
