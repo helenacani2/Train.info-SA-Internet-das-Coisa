57af62c17af2918dc8f98621584eeae1afeb16db
Rafaelfr4nklin <rafael_f_de-farias@estudante.sesisenai.org.br>
2025-11-04 10:16:04 -0300

    Sim

--- S2/S2.ino
+++ S2/S2.ino
@@ -3,9 +3,16 @@
 #include <env.h>
 #include <WiFiClientSecure.h>
 
+//Sensores que tem que publicar são 
+//"presença 2" "presença 4"
+
+//Atuadores para ser utilizado são 
+//LED iluminação 
+//LED status(RGB)
+//Servo 3
+
 WiFiClientSecure client;
 PubSubClient mqtt(client);
-
 
 const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
@@ -16,7 +23,7 @@ const String brokerPass = "";
 
 void setup() {
   Serial.begin(115200);
-  wifi_client.serInsecure();
+  client.setInsecure();
 
 pinMode(2, OUTPUT);
 
@@ -35,6 +42,17 @@ ClientID += String(random(0xffff),HEX);
 while (mqtt.connect(ClientID.c_str()) == 0) {
 Serial.print(".");
 delay(200);
+pinMode(4, OUTPUT);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println("conectando no WiFi");
+  while(WiFi.status()!= WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+  // put your setup code here, to run once:
+
+Serial.println("Conectado com sucesso!");
 }
 mqtt.subscribe(topic.c_str());
 mqtt.setCallback(callback);
@@ -49,8 +67,8 @@ void loop() {
     mensagem = Serial.readStringUntil('\n');
     Serial.println("A mensagem foi: ");
     Serial.println(mensagem);
-    mensagem = "Rafa: " + mensagem;
-    mqtt.publish("brag1", mensagem.c_str());
+    mensagem = "" + mensagem;
+    mqtt.publish(TOPIC_PRESENCE_2, mensagem.c_str());
 
 
   }
@@ -64,5 +82,3 @@ for(int i = 0; i < length; i++) {
 }
 Serial.println(mensagemRecebida);
 }
-
-
