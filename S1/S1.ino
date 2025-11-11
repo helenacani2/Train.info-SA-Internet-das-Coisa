4465a102004148e04346a4304b4325ea89c0350f
helenacani2 <helena_cani@estudante.sesisenai.org.br>
2025-11-11 10:13:25 -0300

    Todos os sensores enviam valores para o servidor - placa S1

--- S1/S1.ino
+++ S1/S1.ino
@@ -4,20 +4,31 @@
 #include "env.h"
 #include <DHT.h>
 
-#define pinLDR 34       //ldr
-#define pinDHT 13       // Pino digital do ESP32 para o DHT
-#define DHTTYPE DHT11   // Tipo de sensor: DHT11
-#define pinPIR 25      // Pino digital para o Sensor PIR
+/*
+4       / DHT
+23     / Ultra_echo
+22     / Ultra_trig
+34     / LDR
+*/
 
-const float LIMITAR_TEMPERATURA = 28.0; // acima de 28.0°C é considerado "Quente"
-const float LIMITAR_UMIDADE = 60.0;     // acima de 60.0% é considerado "Úmido"
+#define led 19
+#define pinLDR 34      //ldr
+#define pinDHT 4       // Pino digital do ESP32 para o DHT
+#define DHTTYPE DHT11  // Tipo de sensor: DHT11
+
+#define TRIGGER_PIN 22
+#define ECHO_PIN 23
+
+//no fim, não vou usar
+const float LIMITAR_TEMPERATURA = 28.0;  // acima de 28.0°C é considerado "Quente"
+const float LIMITAR_UMIDADE = 60.0;      // acima de 60.0% é considerado "Úmido"
 
 DHT dht(pinDHT, DHTTYPE);
 
 WiFiClientSecure wifi_client;
 PubSubClient mqtt(wifi_client);
 
-const String brokerUser = ""; //na SA vai ter, já que esse é publico e a SA não
+const String brokerUser = "";  //na SA vai ter, já que esse é publico e a SA não
 const String brokerPass = "";
 
 void setup() {
@@ -25,28 +36,32 @@ void setup() {
 
   wifi_client.setInsecure();
 
-    pinMode(2, OUTPUT);
-    dht.begin();
-    pinMode(pinPIR, INPUT);
+  pinMode(2, OUTPUT);
+  dht.begin();
+
+  pinMode(19, OUTPUT);
+
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   Serial.println("Conectando no WiFi");
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
 
   Serial.println("Conectado com sucesso!");
 
-  mqtt.setServer(BROKER_URL, BROKER_PORT); //isso aí cria um nome aleatório que aparece pra plaquinha, que é "S1-" + um código aleatório qualquer
+  mqtt.setServer(BROKER_URL, BROKER_PORT);  //isso aí cria um nome aleatório que aparece pra plaquinha, que é "S1-" + um código aleatório qualquer
   String clientID = "S1-";
-  clientID += String(random(0xffff),HEX);
+  clientID += String(random(0xffff), HEX);
   Serial.println("Conectando ao broker...");
-  while (mqtt.connect (clientID.c_str () , BROKER_USR_ID, BROKER_USR_PASS) == 0 ){ //aqui o cod do iago
+  while (mqtt.connect(clientID.c_str(), BROKER_USR_ID, BROKER_USR_PASS) == 0) {  //aqui o cod do iago
     Serial.println(".");
     delay(200);
   }
-  mqtt.subscribe(TOPIC_PRESENCE_1); //conectando a inscrição no tópico com outra placa de msm tópico
+  mqtt.subscribe(TOPIC_PRESENCE_1);  //conectando a inscrição no tópico com outra placa de msm tópico
   mqtt.subscribe(TOPIC_UMID_1);
   mqtt.subscribe(TOPIC_TEMP_1);
   mqtt.subscribe(TOPIC_LUMI_1);
@@ -55,54 +70,85 @@ void setup() {
   Serial.println("\nConectado ao broker!");
 }
 
-void loop() {
-    int luz = map(analogRead(pinLDR),0,4095,0,100);
-    if(luz > 50){
-      mqtt.publish(TOPIC_LUMI_1,"Claro");
-    }else{
-      mqtt.publish(TOPIC_LUMI_1,"Escuro");
-    }
+//função de ler distância:
+long lerDistancia() {
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
 
-    mqtt.loop();
-    delay(500);
+  long duracao = pulseIn(ECHO_PIN, HIGH);
+  long distancia = duracao * 349.24 / 2 / 10000;
 
-  float t = dht.readTemperature();
-  if (t > LIMITAR_TEMPERATURA){
-    mqtt.publish(TOPIC_TEMP_1,"Quente");
-  } else {
-    mqtt.publish(TOPIC_TEMP_1,"Frio");
-  }
-  //adicionar o dado com um novo tópico, para o site
-  
-    mqtt.loop();
-    delay(500);
-
-  float h = dht.readHumidity();
-    if (h > LIMITAR_UMIDADE){
-    mqtt.publish(TOPIC_UMID_1,"Úmido");
-  } else {
-    mqtt.publish(TOPIC_UMID_1,"Seco");
-//adicionar o dado com um novo tópico, para o site
-
-  }
-
-    mqtt.loop();
-    delay(500);
-
-    int presenca = digitalRead(pinPIR);
-    if(presenca == HIGH){
-      mqtt.publish(TOPIC_PRESENCE_1,"Presente");
-    }else{
-      mqtt.publish(TOPIC_PRESENCE_1,"Ausente");
-    }
-
-    mqtt.loop();
-    delay(500);
+  return distancia;
 }
 
-void callback(char * topic, byte* payload, unsigned long length) { //callback para a conexão de inscrição
 
- /* String MensagemRecebida = "";
+void loop() {
+  int luz = map(analogRead(pinLDR), 0, 4095, 0, 100);
+  if (luz > 50) {
+    mqtt.publish(TOPIC_LUMI_1, "Claro");
+    digitalWrite(19, LOW);
+  } else {
+    mqtt.publish(TOPIC_LUMI_1, "Escuro");
+    digitalWrite(19, HIGH);
+  }
+
+  mqtt.loop();
+  delay(500);
+
+  float t = dht.readTemperature();
+  // Converte o float 't' para uma String, depois para const char*
+  String tempString = String(t, 1);
+  mqtt.publish(TOPIC_TEMP_1, tempString.c_str());
+
+  // if (t > LIMITAR_TEMPERATURA){
+  // } else {
+  //   mqtt.publish(TOPIC_TEMP_1,"Frio");
+  // }
+  //adicionar o dado com um novo tópico, para o site
+
+  mqtt.loop();
+  delay(500);
+
+  float h = dht.readHumidity();
+  // Converte o float 'h' para uma String, depois para const char*
+  String umidString = String(h, 1);  // 1 dígito decimal de precisão
+  mqtt.publish(TOPIC_UMID_1, umidString.c_str());
+
+  //   if (h > LIMITAR_UMIDADE){
+  // } else {
+  //   mqtt.publish(TOPIC_UMID_1,"Seco");
+  //adicionar o dado com um novo tópico, para o site
+
+  mqtt.loop();
+  delay(500);
+
+  long distancia = lerDistancia();
+
+  if (distancia < 10) {
+    mqtt.publish(TOPIC_PRESENCE_1, "Presente");
+  } else {
+    mqtt.publish(TOPIC_PRESENCE_1, "Ausente");
+  }
+  delay(500);
+}
+
+/* 
+
+Coisa errada que fiz:
+int presenca = digitalRead(pinPIR);
+if (presenca == HIGH) {
+  mqtt.publish(TOPIC_PRESENCE_1, "Presente");
+} else {
+  mqtt.publish(TOPIC_PRESENCE_1, "Ausente");
+}
+} */
+
+void callback(char* topic, byte* payload, unsigned long length) {  //callback para a conexão de inscrição
+
+  /* String MensagemRecebida = "";
 
   for(int i = 0; i < length; i++){ //pega cada letra de payload e junta a mensagem, bit por bit (letra por letra)
     MensagemRecebida += (char) payload[i];
@@ -115,5 +161,4 @@ void callback(char * topic, byte* payload, unsigned long length) { //callback pa
   }else if(MensagemRecebida == "Nome: 0"){
     digitalWrite(2, LOW);
   } */
-
 }
