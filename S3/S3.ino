960c46a2132472babd3238e3635c0bfb518f65b3
andreerfr <andreerci@gmail.com>
2025-11-18 10:13:58 -0300

    Correções da helena

--- S3/S3.ino
+++ S3/S3.ino
@@ -2,15 +2,20 @@
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
 #include "env.h"
+#include <ESP32Servo.h>
 
 WiFiClientSecure wifi_client;
 PubSubClient mqtt(wifi_client);
 
-const String SSID = "";
-const String PASS = "";
+const byte TRIGGER_PIN = 27;
+const byte ECHO_PIN = 14;
 
-const String brokerUser = "";
-const String brokerPass = "";
+Servo servo1;
+const byte SERVO1_PIN = 25;
+Servo servo2;
+const byte SERVO2_PIN = 24;
+
+#define ledPin 19
 
 void setup() {
   wifi_client.setInsecure();
@@ -26,39 +31,76 @@ void setup() {
   String clientID = "S3-";
   clientID += String(random(0xffff), HEX);
   Serial.println("Conectando ao broker...");
-  while(mqtt.connect(clientID.c_str(),BROKER_USR_ID,BROKER_PASS_USR_PASS) == 0) {
+  while (mqtt.connect(clientID.c_str(), BROKER_USR_ID, BROKER_PASS_USR_PASS) == 0) {
     Serial.print(".");
     delay(200);
+
   }
-  //mqtt.subscribe(topic.c_str());
-  //mqtt.setCallback(callback);
+    // SÓ OS SUBSCRIBE: duas presenças rafael, presença do andré e iluminação helena
+    mqtt.subscribe(TOPIC_PRESENCE_2); //feito 
+    mqtt.subscribe(TOPIC_PRESENCE_4); //feito 
+    mqtt.subscribe(TOPIC_PRESENCE_3); //feito
+    mqtt.subscribe(TOPIC_LUMI_1);  //tópico de iluminação da placa 1 - feito
+
+  mqtt.setCallback(callback);
   Serial.println("\nConectado ao broker!");
+
+  servo1.attach(SERVO1_PIN); //helena, lembre-se
+  servo2.attach(SERVO2_PIN);
+
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(19, OUTPUT);
 }
+
 String mensagem = "";
 // mensagem = Serial.readStringUntil('\n');
 //    Serial.print("A mensagem foi: ");
 //   Serial.println(mensagem);
-void loop() {
-  String mensagem = "";
-  if (Serial.available() > 0) {
 
-    if (Serial.available() > 0) {
-      mensagem = Serial.readStringUntil('\n');
-      mqtt.publish(TOPIC_LEDS_3, mensagem.c_str());
-      Serial.println(mensagem);
-    }
+void loop() {
+  long distancia = lerDistancia();
+  if (distancia < 10) {
+    mqtt.publish(TOPIC_PRESENCE_3, "Presente");
+  } else {
+    mqtt.publish(TOPIC_PRESENCE_3, "Ausente");
   }
-    mqtt.loop();
+  mqtt.loop();
+  delay(500);
 }
-/*
+
+//função de ler distância:
+long lerDistancia() {
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+
+  long duracao = pulseIn(ECHO_PIN, HIGH);
+  long distancia = duracao * 349.24 / 2 / 10000;
+
+  return distancia;
+}
+
+
 void callback(char* topic, byte* payload, unsigned long length) {
   String MensagemRecebida = "";
   for (int i = 0; i < length; i++) {
     //pega cada letra de payload e junta na mensagem
     MensagemRecebida += (char)payload[i];
   }
-  //Posso fazer o que quiser com a mensagem
-  Serial.println(MensagemRecebida);
-}
 
-*/
+  if (strcmp(topic, TOPIC_LUMI_1) == 0 && MensagemRecebida == "Escuro") {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("luz ");
+  } else if (strcmp(topic, TOPIC_LUMI_1) == 0 && MensagemRecebida == "Claro") {
+    digitalWrite(ledPin, LOW);
+    Serial.println("chegou aqui");
+  }
+  repetir para presenca 1 2 3  servo.write(angulo);... 90 ou 120
+// else if (strcmp(topic, TOPIC_LUMI_1) == 0 && MensagemRecebida == "Claro") {
+    // digitalWrite(ledPin, LOW);
+    // Serial.println("chegou aqui");
+// }
+}
