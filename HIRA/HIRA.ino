b8b9cbc4f3f96abc696596ff821f91a536053e56
Iago-KK <iago_kleestadt@estudante.sesisenai.org.br>
2025-11-18 10:11:50 -0300

    LEDs de direção do trem

--- HIRA/HIRA.ino
+++ HIRA/HIRA.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include <PubSubClient.h>  //Conecta as bibliotecas
 #include <WiFiClientSecure.h>
-#include "env.h"
+#include "env.h"  //Conecta o env
 
 WiFiClientSecure wifi_client;
 
@@ -14,80 +14,187 @@ const byte redPin = 19;
 const byte greenPin = 21;
 const byte bluePin = 18;
 
+const byte RedPinVel = 23;
+const byte GreenPinVel = 22;
+
+int status = 1;
 void setup() {
 
   Serial.begin(115200);
-  pinMode(19,OUTPUT);
-  pinMode(21,OUTPUT);
-  pinMode(18,OUTPUT);
+  pinMode(19, OUTPUT);
+  pinMode(21, OUTPUT);
+  pinMode(18, OUTPUT);
+
+  pinMode(RedPinVel, OUTPUT);
+  pinMode(GreenPinVel, OUTPUT);
 
   ledcAttach(redPin, 5000, 8);
   ledcAttach(greenPin, 5000, 8);
   ledcAttach(bluePin, 5000, 8);
 
+
   wifi_client.setInsecure();
-  
 
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   Serial.println("Conectando no Wifi");
 
-  while(WiFi.status() != WL_CONNECTED) {
+  int ConectorWifi = 0;
+
+  while ((WiFi.status() != WL_CONNECTED) && (ConectorWifi != 100)) {
+
+    ledcWrite(redPin, 150);
+    ledcWrite(greenPin, 256);
+    ledcWrite(bluePin, 0);
 
     Serial.print(".");
     delay(200);
 
+    ConectorWifi++;
   }
 
-  Serial.println("Conectado com sucesso");
+  if (ConectorWifi == 100) {
 
-  mqtt.setServer(BROKER_URL,BROKER_PORT);
-  //mqtt.setServer(BROKER_URL.c_str(),BROKER_PORT);
+    ledcWrite(redPin, 255);
+    ledcWrite(greenPin, 0);
+    ledcWrite(bluePin, 0);
+
+    Serial.println("Erro ao conectar com o Wi-Fi");
+
+  } else {
+
+    Serial.println("Conectado ao Wi-Fi com sucesso");
+  }
+
+
+
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
 
   String clientID = "HIRA";
-  clientID += String(random(0xffff),HEX);
+  clientID += String(random(0xffff), HEX);
 
   Serial.println("Conectando ao Broker");
 
-    while(mqtt.connect(clientID.c_str(),BROKER_USER_ID,BROKER_PASS_USR_PASS) == 0) {
-      Serial.print(".");
-      delay(200);
-    }
+  int ConectorBroker = 0;
 
-  //mqtt.subscribe("Train.info/S1/T");
-  mqtt.subscribe("Train.info/S3/LED");
-  //mqtt.subscribe(TOPIC_TREM_LEDST.c_str());
+  while ((mqtt.connect(clientID.c_str(), BROKER_USER_ID, BROKER_PASS_USR_PASS) == 0) && (ConectorBroker != 100)) {
+
+    ledcWrite(redPin, 150);
+    ledcWrite(greenPin, 0);
+    ledcWrite(bluePin, 255);
+
+    Serial.print(".");
+    delay(200);
+    ConectorBroker++;
+  }
+
+  if (ConectorBroker == 100) {
+
+    ledcWrite(redPin, 255);
+    ledcWrite(greenPin, 0);
+    ledcWrite(bluePin, 0);
+
+    Serial.println("Erro ao conectar com o Broker");
+
+  } else {
+
+    Serial.println("\nConectado ao Broker com sucesso");
+  }
+
+
+  mqtt.subscribe(TOPIC_TREM_VELOCIDADE);
+
 
   mqtt.setCallback(callback);
-
-  Serial.println("\nConectado ao Broker");
-
 }
 
 
 
 void loop() {
 
-  
+
   //loop é onde os dados são lidos
 
   String mensagem = "";
 
-  if(Serial.available() > 0) {
+  if (Serial.available() > 0) {
 
     mensagem = Serial.readStringUntil('\n');
 
     Serial.println(mensagem);
 
-    //mqtt.publish("TOPICO_PLACEHOLDER", mensagem.c_str());
+    //status = atoi(mensagem);
 
     mqtt.publish("TOPIC_TREM_LEDST", mensagem.c_str());
-
   }
+
+
 
   mqtt.loop();
 
+  if (Serial.available() > 0) {
+
+    String mensagem = Serial.readStringUntil('\n');
+
+    Serial.println(mensagem);
+    status = mensagem.toInt();
+  }
+
+
+  switch (status) {
+
+    case 0:
+
+      ledcWrite(redPin, 255);
+      ledcWrite(greenPin, 0);
+      ledcWrite(bluePin, 0);
+      break;
+
+    case 10:
+
+      ledcWrite(redPin, 150);
+      ledcWrite(greenPin, 256);
+      ledcWrite(bluePin, 0);
+      break;
+
+    case 20:
+
+      ledcWrite(redPin, 150);
+      ledcWrite(greenPin, 0);
+      ledcWrite(bluePin, 255);
+      break;
+
+    case 1:
+
+      ledcWrite(redPin, 0);
+      ledcWrite(greenPin, 255);
+      ledcWrite(bluePin, 0);
+      break;
+
+    case 2:
+
+      ledcWrite(redPin, 0);
+      ledcWrite(greenPin, 190);
+      ledcWrite(bluePin, 255);
+      break;
+
+    default:
+
+      //for (byte i = 0; i < 4; i++) {
+
+      if ((status != 0) && (status != 1) && (status != 2) && (status != 10) && (status != 20)) {
+
+        ledcWrite(redPin, 0);
+        ledcWrite(greenPin, 0);
+        ledcWrite(bluePin, 255);
+        delay(500);
+        ledcWrite(redPin, 0);
+        ledcWrite(greenPin, 0);
+        ledcWrite(bluePin, 0);
+        delay(500);
+      }
+  }
 }
 
 
@@ -98,28 +205,31 @@ void callback(char* topic, byte* payload, unsigned long length) {
 
   String MensagemRecebida = "";
 
-  for(int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++) {
 
-    MensagemRecebida += (char) payload[i];
+    MensagemRecebida += (char)payload[i];
 
   }
+
+  int VelocidadeTrem = MensagemRecebida.toInt();
+
+  if ((strcmp(topic, TOPIC_TREM_VELOCIDADE)) == 0 && VelocidadeTrem >= 0) {
+
+    MensagemRecebida = "Velocidade positiva, igual a: " + MensagemRecebida + "km/h";
+
+    digitalWrite(RedPinVel, false);
+    digitalWrite(GreenPinVel, true);
+
+  } else {
+
+    MensagemRecebida = "Velocidade negativa, igual a: " + MensagemRecebida + "km/h";
+
+    digitalWrite(RedPinVel, true);
+    digitalWrite(GreenPinVel, false);
+
+  }
+
 
   Serial.println(MensagemRecebida);
-
-  if(MensagemRecebida == "1") {
-
-    ledcWrite(redPin, 255);
-    ledcWrite(greenPin, 0);
-    ledcWrite(bluePin, 0);
-
-  }
-
-  if(MensagemRecebida == "0") {
-
-    ledcWrite(redPin, 0);
-    ledcWrite(greenPin, 0);
-    ledcWrite(bluePin, 0);
-
-  }
 
 }
