a98b5bb9b222a6a3c98c1ad0d120174dc3418180
helenacani2 <you@example.com>
2025-11-24 09:26:39 -0300

    Fazendo e finalizando a placa do rafael

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,103 +1,128 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <env.h>
 #include <WiFiClientSecure.h>
+#include "env.h"
 
-//Sensores que tem que publicar são 
+//Sensores que tem que publicar são
 //"presença 2" "presença 4"
 
-//Atuadores para ser utilizado são 
-//LED iluminação 
-//LED status(RGB)
-//Servo 3
+//TOPIC_PRESENCE_2 //esse tem q ter
+//TOPIC_PRESENCE_4 //esse tem q ter
+//TOPIC_LUMI_1     //esse tem q ter
 
-WiFiClientSecure client;
-PubSubClient mqtt(client);
-
-const String brokerURL = "test.mosquitto.org";
-const int brokerPort = 1883;
-const String topic = "brag";
-
-const String brokerUser = "";
-const String brokerPass = "";
+//Atuadores para ser utilizado são
+//LED iluminação
 
 //Pino dos sensores
+#define TRIGGER_PIN_1 22
+#define ECHO_PIN_1 23
 
-#define TRIGGER_PIN 22
-#define ECHO_PIN 23
-
-#define TRIGGER_PIN 18
-#define ECHO_PIN 19
+#define TRIGGER_PIN_2 18
+#define ECHO_PIN_2 19
 
 //Pino do led
-#define LED_PIN 12
+#define ledPin 12
 
-//Topicos de Presença
-#define TOPIC_PRESENCE_2
-#define TOPIC_PRESENCE_4
-#define TOPIC_LEDS_2
-#define TOPIC_LEDSTATUS_LEDS_2
-#define TOPIC_SERVO_3
-
+WiFiClientSecure wifi_client;
+PubSubClient mqtt(wifi_client);
 
 void setup() {
   Serial.begin(115200);
-  client.setInsecure();
+  wifi_client.setInsecure();
 
-pinMode(2, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
+  pinMode(TRIGGER_PIN_1, OUTPUT);
+  pinMode(ECHO_PIN_1, INPUT);
+
+  pinMode(TRIGGER_PIN_2, OUTPUT);
+  pinMode(ECHO_PIN_2, INPUT);
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.println("conectando no WiFi");
-  while(WiFi.status()!= WL_CONNECTED) {
+  Serial.println("Conectando no WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
-  // put your setup code here, to run once:
 
-Serial.println("Conectado com sucesso!");
-mqtt.setServer(BROKER_URL, 8883);
-String ClientID = "S2-";
-ClientID += String(random(0xffff),HEX);
-while (mqtt.connect(ClientID.c_str()) == 0) {
-Serial.print(".");
-delay(200);
-pinMode(4, OUTPUT);
+  Serial.println("Conectado com sucesso!");
 
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.println("conectando no WiFi");
-  while(WiFi.status()!= WL_CONNECTED) {
-    Serial.print(".");
+  mqtt.setServer(BROKER_URL, BROKER_PORT);  //isso aí cria um nome aleatório que aparece pra plaquinha, que é "S2-" + um código aleatório qualquer
+  String clientID = "S2-";
+  clientID += String(random(0xffff), HEX);
+  Serial.println("Conectando ao broker...");
+  while (mqtt.connect(clientID.c_str(), BROKER_USR_ID, BROKER_USR_PASS) == 0) {
+    Serial.println(".");
     delay(200);
   }
-  // put your setup code here, to run once:
 
-Serial.println("Conectado com sucesso!");
+  //conectando a inscrição no tópico com outra placa de msm tópico
+
+  mqtt.subscribe(TOPIC_PRESENCE_2);  //p1
+  mqtt.subscribe(TOPIC_PRESENCE_4);  //p2
+  mqtt.subscribe(TOPIC_LUMI_1);      //tópico de iluminação da placa 1
+
+  mqtt.setCallback(callback);
+  Serial.println("\nConectado ao broker!");
 }
-mqtt.subscribe(topic.c_str());
-mqtt.setCallback(callback);
-Serial.println("\nConectado ao broker!");
 
+//funções para ler a distância e configurar a distância
+
+long lerDistancia1() {
+  digitalWrite(TRIGGER_PIN_1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN_1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN_1, LOW);
+  long duracao = pulseIn(ECHO_PIN_1, HIGH);
+  long distancia1 = duracao * 349.24 / 2 / 10000;
+  return distancia1;
 }
 
+long lerDistancia2() {
+  digitalWrite(TRIGGER_PIN_2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN_2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN_2, LOW);
+  long duracao = pulseIn(ECHO_PIN_2, HIGH);
+  long distancia2 = duracao * 349.24 / 2 / 10000;
+  return distancia2;
+}
+
+//verifica se o trem está presente ou ausente
 void loop() {
-  // put your main code here, to run repeatedly:
-  String mensagem = "";
-  if(Serial.available() > 0) {
-    mensagem = Serial.readStringUntil('\n');
-    Serial.println("A mensagem foi: ");
-    Serial.println(mensagem);
-    mensagem = "" + mensagem;
-    mqtt.publish("TOPIC_PRESENCE_2", mensagem.c_str());
+  long distancia1 = lerDistancia1();
+  long distancia2 = lerDistancia2();
 
-
+  if (distancia1 < 10) {
+    mqtt.publish(TOPIC_PRESENCE_2, "Presente");
+  } else {
+    mqtt.publish(TOPIC_PRESENCE_2, "Ausente");
   }
-    mqtt.loop();
+
+  if (distancia2 < 10) {
+    mqtt.publish(TOPIC_PRESENCE_4, "Presente");
+  } else {
+    mqtt.publish(TOPIC_PRESENCE_4, "Ausente");
+  }
+  mqtt.loop();
+  delay(500);
 }
+
 void callback(char* topic, byte* payload, unsigned long length) {
 
-String mensagemRecebida = "";
-for(int i = 0; i < length; i++) {
-  mensagemRecebida += (char) payload[i];
-}
-Serial.println(mensagemRecebida);
+  String MensagemRecebida = "";
+  for (int i = 0; i < length; i++) {
+    //pega cada letra de payload e junta na mensagem
+    MensagemRecebida += (char)payload[i];
+  }
+
+  if (strcmp(topic, TOPIC_LUMI_1) == 0 && MensagemRecebida == "Escuro") {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("luz ");
+  } else if (strcmp(topic, TOPIC_LUMI_1) == 0 && MensagemRecebida == "Claro") {
+    digitalWrite(ledPin, LOW);
+    Serial.println("chegou aqui");
+  }
 }
